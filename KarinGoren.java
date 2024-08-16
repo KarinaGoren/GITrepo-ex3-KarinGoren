0d7843c6cef8822dcb17808733e7b7d09e332cab
KarinaGoren <shulmankarin@yahoo.com>
2024-08-16 16:15:32 +0300

    updated KarinGoren.java

--- KarinGoren.java
+++ KarinGoren.java
@@ -1,7 +1,7 @@
 public class KarinGoren {
 	public static void main(String[] args){
 		for (int i=1; i<=5; i++) {
-			System.out.println("My Name Is Karin Shulman Goren" +i);
+			System.out.println("My Name Is Karin Goren and I have two cats");
 		}
 	}
 }
